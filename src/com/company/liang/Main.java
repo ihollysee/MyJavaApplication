c55ed2d21b515f7b7c3d369257564934cab16fe0
hongliang.xie <xhl94500@alibaba-inc.com>
2016-03-02 11:41:00 +0800

    gradle配置：添加json的依赖。 复现json字符串中有非法字符造成的崩溃。

--- src/com/company/liang/Main.java
+++ src/com/company/liang/Main.java
@@ -1,8 +1,11 @@
 package com.company.liang;
 
+import org.json.JSONObject;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        String jsonString = "{\"adcode\": 120000, \"name\": \"\\u5929\\u6{\\\"}";
+        JSONObject json = new JSONObject(jsonString);
     }
 }
