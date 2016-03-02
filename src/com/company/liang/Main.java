b93414fd05377caaafd0253ff088ffd4aa560061
hongliang.xie <xhl94500@alibaba-inc.com>
2016-03-02 11:45:18 +0800

    添加合法的json字符串

--- src/com/company/liang/Main.java
+++ src/com/company/liang/Main.java
@@ -5,7 +5,9 @@ import org.json.JSONObject;
 public class Main {
 
     public static void main(String[] args) {
-        String jsonString = "{\"adcode\": 120000, \"name\": \"\\u5929\\u6{\\\"}";
-        JSONObject json = new JSONObject(jsonString);
+        String jsonString = "{\"adcode\": 120000, \"name\": \"\\u5929\\u6{\\\"}";//含有非法字符
+        String jsonStringOk = "{\"adcode\": 120000, \"name\": \"\\u5929\\u5929\"}";
+        JSONObject json = new JSONObject(jsonStringOk);
+        System.out.println(json.toString());
     }
 }
