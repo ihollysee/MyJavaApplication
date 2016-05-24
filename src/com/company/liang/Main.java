410561f349c8f2652c2827edd7a42bf83ae91d47
hongliang.xie <xhl94500@alibaba-inc.com>
2016-05-24 15:53:46 +0800

    替换换行符

--- src/com/company/liang/Main.java
+++ src/com/company/liang/Main.java
@@ -5,17 +5,30 @@ import org.json.JSONObject;
 public class Main {
 
     public static void main(String[] args) {
-        String jsonStringOk = "{\"adcode\": 120000, \"name\": \"\\u5929\\u5929\"}";
-        String jsonString = "{\"adcode\": 120000, \"name\": \"\\u5929\\u6{\"t\"}";//含有非法字符
+//        String jsonStringOk = "{\"adcode\": 120000, \"name\": \"\\u5929\\u5929\"}";
+//        String jsonString = "{\"adcode\": 120000, \"name\": \"\\u5929\\u6{\"t\"}";//含有非法字符
+//
+//        String josnStr = "{[\"\\u6{\"t]}";
+//
+//        JSONObject json = new JSONObject(josnStr);
+//        System.out.println(json.toString());
+//
+//
+//        Boolean data = null;
+//        if (Boolean.TRUE.equals(data)) {//不会崩溃
+//        }
 
-        String josnStr = "{[\"\\u6{\"t]}";
+        String CRLF = "buildNumber:075\n" +
+                "\n" +
+                "AndroidManifest.xml,android:versionName:7.6.6.1075\n" +
+                "\n" +
+                "buildNumberWithoutZero:75\n" +
+                "\n" +
+                "564\n" +
+                "7.6.6.1075\n" +
+                "\n" +
+                "[workspace] ";
 
-        JSONObject json = new JSONObject(josnStr);
-        System.out.println(json.toString());
-
-
-        Boolean data = null;
-        if (Boolean.TRUE.equals(data)) {//不会崩溃
-        }
+        System.out.print(CRLF.replaceAll("(\\r\\n|\\r|\\n|\\n\\r)", ""));
     }
 }
